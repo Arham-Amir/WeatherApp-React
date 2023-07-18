040cc356df4fe459857896805710c25c87a4bc7c
Arham Amir <arhamamir424@gmail.com>
2023-07-18 13:31:25 +0500

    Final Changes, Change the navbar margix for lg window size.

--- src/Components/Base/navbar.jsx
+++ src/Components/Base/navbar.jsx
@@ -12,20 +12,20 @@ export default function Navbar(props) {
   return (
     <section className={`${props.class} lg:h-[100vh] lg:sticky lg:top-0 2xl:text-2xl sm:w-full flex sm:flex-row justify-center lg:py-5 xl:px-2 lg:px-0`} >
       <section className="lg:bg-gray-700  sm:bg-sky-600 lg:w-4/5 sm:w-full rounded-[20px] flex lg:flex-col sm:flex-row items-center py-6 gap-5" >
-        <Logo class="lg:mb-12 lg:block sm:hidden"></Logo>
-        <Page class="lg:mb-4">
-          <Link to="/" className="flex justify-center flex-col ">
+        <Logo class="lg:mb-12 lg:block sm:hidden "></Logo>
+        <Page class="lg:mb-4 lg:w-full">
+          <Link to="/" className="flex justify-center flex-col">
             <FontAwesomeIcon icon={faSmog} style={{ color: "#ffffff", }} />
             <p className={`${props.active == 'Home' ? 'text-white' : 'text-gray-300'} mt-2 mx-auto`}>Weather</p>
           </Link>
         </Page>
-        <Page class="lg:mb-4">
+        <Page class="lg:mb-4 lg:w-full">
           <Link to="/cities" className="flex justify-center flex-col">
             <FontAwesomeIcon icon={faList} style={{ color: "#ffffff", }} />
             <p className={`${props.active == 'Cities' ? 'text-white' : 'text-gray-300'} mt-2 mx-auto`}>Citites</p>
           </Link>
         </Page>
-        <Page class="lg:mb-4">
+        <Page class="lg:mb-4 lg:w-full">
           <FontAwesomeIcon icon={faSliders} style={{ color: "#ffffff", }} />
           <p className={`${props.active == 'Settings' ? 'text-white' : 'text-gray-300'} mt-2 mx-auto`}>Settings</p>
         </Page>
